7543e85cb40b3c2122b4a5743ed9ca1a13fafba8
kashkoin-foundation <36289897+kashkoin-foundation@users.noreply.github.com>
2018-08-19 11:06:04 +0530

    Update pools

--- config.js
+++ config.js
@@ -7,8 +7,10 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "khk": [
-		["youpool.io/KHK", "http://khk.youpool.io:8117/stats"],
-	        ["getpool.org/khk", "https://api.getpool.org/rpc/khk/stats"],
+		        ["youpool.io/KHK", "http://khk.youpool.io:8117/stats"],
+				["Crypto9coin.cf/khk", "https://crypto9coin.cf:30968/stats"],
+				["walpool.com/khk", "https://api.walpool.com/rpc/khk/stats"],
+	            ["getpool.org/khk", "https://api.getpool.org/rpc/khk/stats"],
                 ["easyhash.pro/khk", "https://easyhash.pro/khk/api/stats"]		
            ]
 };
